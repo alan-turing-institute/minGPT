54fbc4eadbdc290152802cd660e174057b7ebaff
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2024-10-31 12:16:29 +0000

    Apply variable learning rate
    Adds a function to adjust the learning rate during training.
    
    Takes us to 2:24:55 in the video:
    
    https://youtu.be/l8pRSuU81PU?t=8695

--- gpt-2-video/train_gpt2.py
+++ gpt-2-video/train_gpt2.py
@@ -246,8 +246,22 @@ model.to(device)
 if device == 'cuda':
     model = torch.compile(model)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
-for i in range(50):
+max_lr = 3e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+def get_lr(it):
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    if it > max_steps:
+        return min_lr
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -258,8 +272,12 @@ for i in range(50):
     else:
         logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     t1 = time.time()
-    dt = (t1 - t0) * 1000
+    dt = (t1 - t0)
     tokens_per_second = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i}, loss {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_second:.2f}")
+    print(f"step {step:4d} | loss {loss.item()} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt * 1000:.2f}ms | tok/sec: {tokens_per_second:.2f}")
