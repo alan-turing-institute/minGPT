8f0317bc3345cacb0ea6b98fc39435103f5fc20c
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2024-11-07 16:40:58 +0000

    Add weight decay and minibatches
    Adds a weight decay optimiser and the ability to split batches into
    minibatches so as to support batch sizes larger than the GPU can support
    in one go.
    
    Takes us to 2:46:53 in the video:
    
    https://youtu.be/l8pRSuU81PU?t=10013

--- gpt-2-video/train_gpt2.py
+++ gpt-2-video/train_gpt2.py
@@ -11,10 +11,11 @@ from torch.nn import functional as F
 import math
 import tiktoken
 import time
+import inspect
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 if torch.backends.mps.is_built():
-    device = torch.device("mps")
+    device = "mps"
 
 # ---------------------------------------------------------
 
@@ -208,6 +209,25 @@ class GPT(nn.Module):
 
         return model
 
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_deay': weight_decay},
+            {'params': nodecay_params, 'weight_deay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and "cuda" in device
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
 class DataLoaderLite:
     def __init__(self, B, T):
         self.B = B
@@ -237,7 +257,15 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
     toch.cuda.manual_seed(1337)
 
-train_loader = DataLoaderLite(B=4, T=32)
+total_batch_size = 524288
+B = 16
+T = 1024
+assert total_batch_size % (B * T) == 0, "make sure total_batch)size is divisible by B * T"
+grad_accum_steps = total_batch_size // (B * T)
+print(f"total desired batch size: {total_batch_size}")
+print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
+
+train_loader = DataLoaderLite(B=16, T=32)
 
 torch.set_float32_matmul_precision('high')
 
@@ -260,18 +288,24 @@ def get_lr(it):
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
     return min_lr + coeff * (max_lr - min_lr)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+
 for step in range(max_steps):
     t0 = time.time()
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    if device == "cuda":
-        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        if device == "cuda":
+            with torch.autocast(device_type=device, dtype=torch.bfloat16):
+                logits, loss = model(x, y)
+        else:
             logits, loss = model(x, y)
-    else:
-        logits, loss = model(x, y)
-    loss.backward()
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     lr = get_lr(step)
     for param_group in optimizer.param_groups:
@@ -279,5 +313,6 @@ for step in range(max_steps):
     optimizer.step()
     t1 = time.time()
     dt = (t1 - t0)
+    tokens_processed = train_loader.B + train_loader.T * grad_accum_steps
     tokens_per_second = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {step:4d} | loss {loss.item()} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt * 1000:.2f}ms | tok/sec: {tokens_per_second:.2f}")
+    print(f"step {step:4d} | loss {loss_accum.item()} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt * 1000:.2f}ms | tok/sec: {tokens_per_second:.2f}")
